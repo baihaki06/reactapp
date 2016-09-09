981565ce4eb736b6735f33e141dc11181a730992
baihaki06 <baihaki0607@gmail.com>
2016-09-09 16:52:56 +0700

    start app react

---
+++ App.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var React = require('react');
+
+var App = React.createClass({
+
+	render: function() {
+		return (
+			<div > contoh </div>
+		);
+	}
+
+});
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+	entry : './main.js',
+	output: {
+		part : './',
+		filename: 'index.js'
+	},
+	devServer :{
+		inline :true,
+		port:3333
+	},
+	module :{
+		loaders :[
+			{
+				test:/\.js$/,
+				exclude : /node_modules/,
+				loader : 'babel',
+				query : {
+					presets :['es2015', 'react']
+				}
+			}
+
+		]
+	}
+
+}
